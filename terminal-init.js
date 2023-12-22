b9c7b94865d3a6f841417208f35f7ce479d08d58
zangston <winston.y.zhang@gmail.com>
2023-12-22 03:36:00 -0500

    Move terminal-init code to main

--- terminal-init.js
+++
@@ -1,5 +0,0 @@
-function terminalInit(xterm) {
-    xterm.open(document.getElementById('terminal'));
-    xterm.resize(34, 22);
-    xterm.write("\x1B[0;32m");   // Note: colors dictated by ANSI scheme: https://bixense.com/clicolors/ 
-}
--- terminal-main.js
+++ terminal-main.js
@@ -1,3 +1,5 @@
 var xterm = new Terminal();
-terminalInit(xterm);
+xterm.open(document.getElementById('terminal'));
+xterm.resize(34, 22);
+xterm.write("\x1B[0;32m");   // Note: colors dictated by ANSI scheme: https://bixense.com/clicolors/ 
 xterm.write("fuck fuck fuck")
