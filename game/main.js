abf558f6bf2b521100cc6b295df05efd15476f3f
zangston <winston.y.zhang@gmail.com>
2023-12-23 05:20:39 -0500

    Revise board formatting for readability

--- game/main.js
+++ game/main.js
@@ -25,28 +25,28 @@
                   \/\/\/\/\/\/\/\/\/\/
 */
 var board = [];
-board[0]    = "                <! . . . . . . . . . .!>    SCORE\n\r";
-board[1]    = "                <! . . . . . . . . . .!>    \n\r";
-board[2]    = "                <! . . . . . . . . . .!>\n\r";
-board[3]    = "                <! . . . . . . . . . .!>    LEFT/RIGHT: Move piece\n\r";
-board[4]    = "                <! . . . . . . . . . .!>    X: Rotate clockwise\n\r";
-board[5]    = "                <! . . . . . . . . . .!>    Z: Rotate counterclockwise\n\r";
-board[6]    = "    NEXT        <! . . . . . . . . . .!>    Down: Soft drop\n\r";
-board[7]    = "                <! . . . . . . . . . .!>    Space: Hard drop\n\r";
-board[8]    = "                <! . . . . . . . . . .!>    Shift: Hold piece\n\r";
-board[9]   = "                <! . . . . . . . . . .!>\n\r";
-board[10]   = "                <! . . . . . . . . . .!>\n\r";
-board[11]   = "                <! . . . . . . . . . .!>\n\r";
-board[12]   = "    HOLD        <! . . . . . . . . . .!>\n\r";
-board[13]   = "                <! . . . . . . . . . .!>\n\r";
-board[14]   = "                <! . . . . . . . . . .!>\n\r";
-board[15]   = "                <! . . . . . . . . . .!>\n\r";
-board[16]   = "                <! . . . . . . . . . .!>\n\r";
-board[17]   = "                <! . . . . . . . . . .!>\n\r";
-board[18]   = "                <! . . . . . . . . . .!>\n\r";
-board[19]   = "                <! . . . . . . . . . .!>\n\r";
-board[20]   = "                <! . . . . . . . . . .!>\n\r";
-board[21]   = "                <!====================!>\n\r";
+board[0]    = "                <! . . . . . . . . . .!>    SCORE                        \n\r";
+board[1]    = "                <! . . . . . . . . . .!>                                 \n\r";
+board[2]    = "                <! . . . . . . . . . .!>                                 \n\r";
+board[3]    = "                <! . . . . . . . . . .!>    LEFT/RIGHT: Move piece       \n\r";
+board[4]    = "                <! . . . . . . . . . .!>    X: Rotate clockwise          \n\r";
+board[5]    = "                <! . . . . . . . . . .!>    Z: Rotate counterclockwise   \n\r";
+board[6]    = "    NEXT        <! . . . . . . . . . .!>    Down: Soft drop              \n\r";
+board[7]    = "                <! . . . . . . . . . .!>    Space: Hard drop             \n\r";
+board[8]    = "                <! . . . . . . . . . .!>    Shift: Hold piece            \n\r";
+board[9]    = "                <! . . . . . . . . . .!>                                 \n\r";
+board[10]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[11]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[12]   = "    HOLD        <! . . . . . . . . . .!>                                 \n\r";
+board[13]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[14]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[15]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[16]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[17]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[18]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[19]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[20]   = "                <! . . . . . . . . . .!>                                 \n\r";
+board[21]   = "                <!====================!>                                 \n\r";
 board[22]   = "                  \\/\\/\\/\\/\\/\\/\\/\\/\\/\\/";
 
 
