476d5bd3d75cfeb2de380b07727c8c89559e7b04
zangston <winston.y.zhang@gmail.com>
2024-06-06 20:17:07 -0400

    Attempt at moving startup animation to own file

--- game/main.js
+++ game/main.js
@@ -1,3 +1,5 @@
+import { startUpAnimation } from '/.startUpAnimation.js';
+
 // Game board represented as an array of strings, one string per line
 // Example board:
 /*
@@ -61,36 +63,13 @@ var startupPrintSpeed = 100;
  * @param {*} xterm - xterm instance
  */
 async function main(xterm) {
-    await startUpAnimation(xterm);
+    await startUpAnimation(xterm, board, startupPrintSpeed);
 
     setTimeout(() => {
         spawnBlock();
 
         updateScreen(xterm);
     }, startupPrintSpeed * 35);
-}
-
-/**
- * Prints new game-state/board-snapshot to the terminal with a loading effect
- * @param {*} xterm - xterm instance passed from main function
- */
-async function startUpAnimation(xterm) {
-    xterm.write("\n\r"); // top margin line
-    xterm.write("\n\r"); // top margin line
-
-    let i = 0;
-
-    function printLine() {
-        if (i < board.length) {
-            xterm.write(board[i]);
-            i++;
-            setTimeout(printLine, startupPrintSpeed); // Adjust the delay (in milliseconds) as needed
-        } else {
-            xterm.write("\n\r"); // bottom margin line
-        }
-    }
-
-    printLine();
 }
 
 /**
